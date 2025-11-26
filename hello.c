e7dc518be321e89aea3580614118814d327e3a17
Phil <philip.guglielmi@tdsb.on.ca>
2025-11-26 10:19:26 -0500

    Adding hello world program

--- hello.c
+++ hello.c
@@ -1,8 +1,14 @@
+/*
+  author: Mr. Guglielmi
+  Date: 2025/11/25
+  Description: A simple hello world program
+*/
+
 #include <stdio.h>
 
 int main(){
 
-  puts ("Hello world!");
+  puts ("Hello world!"); //display 'hello world!' to the terminal
 
   return 0;
 
